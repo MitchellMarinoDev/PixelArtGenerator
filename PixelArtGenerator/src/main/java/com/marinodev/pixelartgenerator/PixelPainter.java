428dc77160c5b1759ab93a1a28f9368b7e67a501
MitchellMarinoDev <MitchellMarinoDev@users.noreply.github.com>
2021-01-06 23:34:02 -0600

    Added Support for "Drag-Painting"

--- PixelArtGenerator/src/main/java/com/marinodev/pixelartgenerator/PixelPainter.java
+++ PixelArtGenerator/src/main/java/com/marinodev/pixelartgenerator/PixelPainter.java
@@ -1,5 +1,6 @@
 package com.marinodev.pixelartgenerator;
 
+import javax.swing.*;
 import java.util.List;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -12,13 +13,28 @@ public abstract class PixelPainter extends MouseAdapter {
     private final List<Consumer<Pixel>> button2Listeners = new ArrayList<>();
     private final List<Consumer<Pixel>> button3Listeners = new ArrayList<>();
 
+    private int lastMouseButton;
+
     public PixelPainter(PixelArtPanel panel) {
         super();
         panel.addMouseListener(this);
+        panel.addMouseMotionListener(this);
         this.panel = panel;
     }
 
     public void onRebuildPixels() {}
+
+    @Override
+    public void mouseDragged(MouseEvent event) {
+        Pixel pixel = panel.getPixel(event.getX() / panel.getPixelSize(), event.getY() / panel.getPixelSize());
+        System.out.println(event.getButton());
+        if (SwingUtilities.isLeftMouseButton(event))
+            button1Listeners.forEach(pixelConsumer -> pixelConsumer.accept(pixel));
+        else if (SwingUtilities.isMiddleMouseButton(event))
+            button2Listeners.forEach(pixelConsumer -> pixelConsumer.accept(pixel));
+        else if (SwingUtilities.isRightMouseButton(event))
+            button3Listeners.forEach(pixelConsumer -> pixelConsumer.accept(pixel));
+    }
 
     @Override
     public void mousePressed(MouseEvent event) {
